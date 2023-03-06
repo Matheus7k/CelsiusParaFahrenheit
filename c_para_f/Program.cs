7bc6aa4968e21cce33940419915d2fcd2b42c7e3
Matheus7k <06matheus.oliveira@gmail.com>
2023-03-05 23:17:26 -0300

    Add project files.

---
+++ c_para_f/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static double ConverterParaFahrenheit(double temperaturaC)
+    {
+        return ((temperaturaC * 9) / 5) + 32;
+    }
+
+    private static void Main(string[] args)
+    {
+        double tc, tf;
+
+        Console.WriteLine("Informe a temperatura: ");
+        tc = double.Parse(Console.ReadLine());
+
+        tf = ConverterParaFahrenheit(tc);
+
+        Console.WriteLine($"A temperatura {tc} em gruas Celsius, é {tf.ToString("F2")} em graus Fahrenheit!");
+    }
+}
